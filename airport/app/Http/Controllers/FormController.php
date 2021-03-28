2134addcab4b44b428bc60db47d2b7c8788a4c83
Giovanni Scotti <scottigiovanni93@gmail.com>
2021-03-28 17:12:18 +0200

    create route form

--- airport/app/Http/Controllers/FormController.php
+++ airport/app/Http/Controllers/FormController.php
@@ -6,5 +6,70 @@ use Illuminate\Http\Request;
 
 class FormController extends Controller
 {
-    //
+    public function index(Request $req)
+    {
+        $airports = [
+                [
+                    'id' => 0,
+                    'nome' => 'Catania',
+                    'code' => 'aaa'
+                ],
+                [
+                    'id' => 1,
+                    'nome' => 'Londra',
+                    'code' => 'bbb'
+                ],
+                [
+                    'id' => 2,
+                    'nome' => 'Berlino',
+                    'code' => 'ccc'
+                ],
+                [
+                    'id' => 3,
+                    'nome' => 'Milano',
+                    'code' => 'ddd'
+                ]
+        ];
+        $flights = [
+            [
+                'code_departure' => 'aaa',
+                'code_arrival' => 'bbb',
+                'price' => 20
+            ],
+            [
+                'code_departure' => 'bbb',
+                'code_arrival' => 'ccc',
+                'price'=> 40
+            ],
+            [
+                'code_departure' => 'ccc',
+                'code_arrival' => 'ddd',
+                'price' => 70
+            ],
+            [
+                'code_departure' => 'ddd',
+                'code_arrival' => 'aaa',
+                'price' => 65
+            ]
+        ];
+
+        foreach ($flights as $flight) {
+            $price = $flight['price'];
+            break;
+        };
+        for ($i=0; $i <2 ; $i++) {
+            $var_departure = $_POST['departure'];
+            $var_arrival = $_POST['arrival'];
+            echo $var_departure . '-'. $var_arrival;
+            if ($var_departure == "Catania" && $var_arrival =="Londra") {
+                echo ' scalo '. ' : '. '' . '<option value="Berlino">Berlino</option>';
+            } elseif ($var_departure == "Catania" && $var_arrival =="Milano") {
+                echo '<br>';
+                echo ' scalo '. ' : '. '' . '<option value="Londra">Londra</option>';
+                echo 'prezzo del volo è '. $price .'<br>';
+            } elseif($var_departure == "Catania" && $var_arrival =="Berlino"){
+                echo ' scalo '. ' : '. '' . '<option value="Londra">Londra</option>';
+            }
+        }
+    }
 }
--- airport/routes/web.php
+++ airport/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FormController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,3 +20,5 @@ Route::get('/', function () {
 Route::get('/form', function() {
     return view('form');
 })->name('form');
+
+Route::post("form",[FormController::class,'index']);
