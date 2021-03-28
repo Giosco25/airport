401ae9fc1aa3dc83cb68c26ee63d74ac13e0920b
Giovanni Scotti <scottigiovanni93@gmail.com>
2021-03-28 16:54:28 +0200

    create file php into config

---
+++ airport/config/airport.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    'nomi_airport' => [
+        [
+            'nome' => 'Catania',
+            'code' => 'aaa'
+        ],
+        [
+            'nome' => 'Londra',
+            'code' => 'bbb'
+        ],
+        [
+            'nome' => 'Berlino',
+            'code' => 'ccc'
+        ],
+        [
+            'nome' => 'Milano',
+            'code' => 'ddd'
+        ]
+    ]
+];
+
+?>
